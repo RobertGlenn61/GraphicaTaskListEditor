41a88edbf7247c406232969dce3501651ee2531a
RobertGlenn61 <rc_glenn@hotmail.com>
2024-08-17 15:56:12 -0700

    Added GUI + all buttons + clock

--- main.py
+++ main.py
@@ -1,12 +1,68 @@
 # Create a simple grahpical task list editor
 from FileFunctions import read_todos, write_todos
+import FreeSimpleGUI as gui
+import time
 
-menu = 'Enter a commnad: View, Edit, Add, Complete, Exit: '
-command = ''
-task_list = ''
+
+gui.theme('Purple')
+task_label = gui.Text('Type in a task')
+task_text = gui.InputText(tooltip="Enter Task", key='task')
+add_button = gui.Button( 'Add')
+task_list = gui.Listbox(read_todos(), key='task_list', enable_events=True,size=[45,10])
+edit_button = gui.Button('Edit')
+complete_button = gui.Button('Complete')
+exit_button = gui.Button('Exit')
+time_label = gui.Text(time.strftime('%A %B %d, %Y %I:%M:%S %p'), key='clock')
+
+window = gui.Window('My Task List',
+                    layout= [[time_label],
+                             [task_label],
+                             [task_text, add_button],
+                             [task_list, edit_button,complete_button],
+                             [exit_button]],
+                    font=('Helvetica', 20))
 
 while True:
-    command = input(menu).strip('\n')
+    event, values = window.read( timeout=500)
+    #print(1,event)
+    #print(2, values)
+    #print(3, values['task_list'])
+    window['clock'].update(value = time.strftime('%A %B %d, %Y %I:%M:%S %p'))
+    match event:
+        case 'Add':
+            list = read_todos()
+            list.append(values['task'] + '\n')
+            write_todos(list)
+            window['task_list'].update(values=list)
+        case 'Complete':
+            try:
+                list = read_todos()
+                list.remove(values['task_list'][0])
+                write_todos(list)
+                window['task_list'].update(values=list)
+                window['task'].update('')
+            except IndexError:
+                gui.popup('Pleae select a task', title='Error', font=('Helvetica', 20))
+        case 'Edit':
+            try:
+                list = read_todos()
+                new_task = values['task']
+                index = list.index(values['task_list'][0])
+                list[index] = new_task +'\n'
+                write_todos(list)
+                window['task'].update('')
+                window['task_list'].update(values=list)
+            except IndexError:
+                gui.popup('Pleae select a task', title='Error', font=('Helvetica', 20))
+        case 'task_list':
+            window['task'].update(value=values['task_list'][0])
+        case 'Exit':
+            break
+
+
+'''
+while True:
+
     match command.upper():
         case 'VIEW' | 'V':
             task_list = read_todos()
@@ -28,5 +84,5 @@ while True:
             write_todos(task_list)
         case 'EXIT' | 'X':
             break
-
+'''
 print("Goodbye")
